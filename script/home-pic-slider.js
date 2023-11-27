69c7d27fdefaad4e937486e84ce1bc196672b63a
taibru <104684750+taibru@users.noreply.github.com>
2023-11-27 13:49:45 +0530

    Update home-pic-slider.js

--- script/home-pic-slider.js
+++ script/home-pic-slider.js
@@ -2,16 +2,21 @@ const backBtn = document.querySelector('.back');
 const forBtn = document.querySelector('.forward');
 const sliderEl = document.querySelector('#slider-home');
 const bannerEl = document.querySelectorAll('.banner-section');
+let timeout;
 
 console.log(sliderEl);
 let imgCount = 1;
 
+updateMove();
+
 backBtn.addEventListener('click', ()=>{
   imgCount--;
+  clearTimeout(timeout);
   updateMove();
 });
 forBtn.addEventListener('click', ()=>{
   imgCount++;
+  clearTimeout(timeout);
   updateMove();
   
 });
@@ -23,4 +28,9 @@ function updateMove(){
     imgCount = 1;
   }
   sliderEl.style.transform = `translateX(-${(imgCount-1)*100}vw)`;
+  
+  timeout = setTimeout(()=>{
+    imgCount++;
+    updateMove();
+  }, 2000);
 }
